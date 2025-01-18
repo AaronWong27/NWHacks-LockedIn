17bab92d91c5ef33ffa08c8acf2fc43e7abc7114
fbd1 <git@github.com>
2025-01-18 15:49:42 -0800

    Removing default react code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState("");
+  
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("Error:", err));
-  }, []);
-
-  return <h1>{message}</h1>;
+  return 
+    <>
+      <p>Hello</p>
+    </>;
 }
 
 export default App;
