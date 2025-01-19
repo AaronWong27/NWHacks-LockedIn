3103b638bca25fa159bc3b4e7d5ff7b81d3f7d44
fbd1 <git@github.com>
2025-01-18 16:10:02 -0800

    Added Router to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Index from "./Index.jsx";
+
 import './css/App.css'
 
 function App() {
   
 
-  return 
-    <>
-      <p>Hello</p>
-    </>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
