6bc226278b28f32d716acd57ff565c93d8b2433d
fbd1 <git@github.com>
2025-01-18 16:54:07 -0800

    Added image of Solo waving with alt text

--- src/Index.jsx
+++ src/Index.jsx
@@ -5,6 +5,7 @@ import './css/Index.css'
 
 // Images
 import logo from './assets/lockedin-logo.png'
+import wave from './assets/solo-wave.png'
 
 function Index(){
     return(
@@ -17,16 +18,25 @@ function Index(){
                     </Link>
                 </nav>
 
-                {/* Heading */}
-                <h1 class="font-semibold text-[2.5rem] mt-[2rem]">
-                    Hello,&nbsp; 
-                    <span id="username" class="dark-blue">
-                        Yonka!</span>
-                </h1>
-                {/* Subheading */}
-                <p>
-                    Nice to meet you! I'm here to help you <span class="font-bold">lock in</span> and get a job!
-                </p>
+                <div class="flex">
+                    <div>
+                        {/* Heading */}
+                        <h1 class="font-semibold text-[2.5rem] mt-[2rem]">
+                            Hello,&nbsp; 
+                            <span id="username" class="dark-blue">
+                                Yonka!</span>
+                        </h1>
+                        {/* Subheading */}
+                        <p>
+                            Nice to meet you! I'm here to help you <span class="font-bold">lock in</span> and get a job!
+                        </p>
+                    </div>
+
+                    <img src={wave} alt="Cartoon-styled blue owl raising one arm up in a wave, with its mouth wide open in a smile." />
+                </div>
+
+
+            
             </header>
 
 
